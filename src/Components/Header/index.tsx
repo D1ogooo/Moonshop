d0cec1416cc8ebb8677559f4e8bc210b317bc549
D1ogooo <diiogocosta11@gmail.com>
2024-08-05 22:24:05 -0300

    [fix] menu corrigido

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,16 +1,33 @@
-import { useState, useEffect } from 'react'
-import { HeaderDesktop } from '../HeaderDesktop'
-import { HeaderMobille } from '../HeaderMobille'
+import { External, HeaderStyle, HR } from './style'
+import { Link } from '../LinkDiscord/index'
+import MoonIcon from '/Moonshop icon.png'
 
 export function Header () {
-  const [state, setState] = useState(window.innerWidth)
-
-  useEffect(() => {
-    const handleResize = () => {
-     setState(window.innerWidth)
-    }
-    window.addEventListener('resize', handleResize);
-   },[])
-
-  return ( <> { state > 1100 ? <HeaderDesktop /> : <HeaderMobille /> } </>)
+  return (
+   <>
+    <External>
+     <HeaderStyle>
+      <div className='logoIcon'>
+       <img src={MoonIcon}/>
+      </div>
+       <ul>
+        <li>
+         <a href="#">Início</a>
+        </li>
+        <li>
+         <a href="#TitleSobre">Sobre</a>
+        </li>
+        <li>
+         <a href="#TitlePlanos">Nossos planos</a>
+        </li>
+        <li>
+         <a href="#TitleFaleConosco">Fale conosco</a>
+        </li>
+       </ul>
+       <Link Text={"Comunidade"}/>
+      </HeaderStyle>
+     </External>
+    <HR />
+   </>
+  )
 }
